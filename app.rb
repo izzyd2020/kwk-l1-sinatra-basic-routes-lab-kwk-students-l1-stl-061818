1cd53333ed0e741e774773479232930423e2226e
Isabella Duran <2020ikd7@gmail.com>
2018-06-26 16:27:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,6 +6,6 @@ class App < Sinatra::Base
  "My Home town St.Louis"
  end
  get '/about' do 
- "About my Home town St.Louis"
+ "About my Home town St.Louis has a very exciting place called the arch "
  end
  end
