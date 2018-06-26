5470ef175c64a5919a42eb67229ef1b42878ec32
Isabella Duran <2020ikd7@gmail.com>
2018-06-26 16:21:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-get '/' do 
-  "Avocados and bok choy are the best"
-end
- get '/nutritious' do
-"lots of fiber"
-
+ get '/' do 
  end
-end
+
+# get '/' do 
+#   "Avocados and bok choy are the best"
+# end
+# get '/nutritious' do
+# "lots of fiber"
+
+# end
+# end
